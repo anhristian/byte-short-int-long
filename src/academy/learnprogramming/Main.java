0e65b3077f90a6807f016d366b6a3e2251bd3414
Anastasia Hristian <an.hristian@gmail.com>
2020-08-14 15:55:19 -0600

    Worked with short, byte and long values.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -12,5 +12,40 @@ public class Main {
       System.out.println("Busted Max value = " + (myMaxIntValue + 1));
       System.out.println("Busted Min value = " + (myMinIntValue - 1));
 
+      int myMaxIntTest = 2_147_483_647;
+
+      byte myMinByteValue = Byte.MIN_VALUE;
+      byte myMaxByteValue = Byte.MAX_VALUE;
+      System.out.println("Byte Minimum Value = " + myMinByteValue);
+      System.out.println("Byte Maximum value = " + myMaxByteValue); ;
+
+      short myMinShortValue = Short.MIN_VALUE;
+      short myMaxShortValue = Short.MAX_VALUE;
+      System.out.println("Short Minimum Value = " + myMinShortValue);
+      System.out.println("Short Maximum value = " + myMaxShortValue); ;
+
+      long myLongValue = 100;
+      long myMinLongValue = Long.MIN_VALUE;
+      long myMaxLongValue = Long.MAX_VALUE;
+      System.out.println("Long Minimum Value = " + myMinLongValue);
+      System.out.println("Long Maximum value = " + myMaxLongValue); ;
+      long bigLongLiteralValue = 2_147_483_647_234L;
+      System.out.println(bigLongLiteralValue);
+
+      short bigShortLiteralValue = 32767;
+
+      int myTotal = (myMinIntValue / 2);
+
+      byte myNewByteValue = (byte) (myMinByteValue / 2);
+
+      short myNewShortValue = (short) (myMinShortValue / 2);
+
+
+      int myVal = 15;
+      long myLongVal = 50000L + 10 * (myMaxByteValue + myMaxShortValue + myMaxIntValue);
+      System.out.println(myLongVal);
     }
+
+
+
 }
